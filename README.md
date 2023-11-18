3c55d8a8236574383cfd98fa12b02f875b0de49f
gmerveartar <111999100+gmerveartar@users.noreply.github.com>
2023-11-18 22:21:59 +0300

    Update

---
+++ README.md
@@ -0,0 +1,2 @@
+This Repository consists of my studies and reviews during the Java Application Development Course of C and System Programmers Association
+Instructor : Oguz Karan 
---
+++ src/SampleCodes/Projects/001_DemoMavenLocalRepoApp/src/main/java/com/artardev/Application.java
@@ -0,0 +1,10 @@
+package com.artardev;
+
+import com.karandev.util.console.Console;
+
+class Application {
+    public static void run(String[] args)
+    {
+        Console.writeLine("Hello, World");
+    }
+}

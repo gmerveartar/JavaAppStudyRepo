d4106bf9907365b0851dba4e846f64868362fb6a
gmerveartar <gmerveartar@gmail.com>
2023-11-24 15:58:59 +0300

    003_DemoLibraryUsageMavenRemote is added to Projects folder

---
+++ Projects/003_DemoLibraryUsageMavenRemote/src/main/java/com/artardev/Application.java
@@ -0,0 +1,12 @@
+package com.artardev;
+
+import com.karandev.io.util.console.Console;
+class Application {
+    public static void run(String[] args)
+    {
+        int a = Console.readInt("Input first number:", "Invalid entry");
+        int b = Console.readInt("Input second number:", "Invalid entry");
+
+        Console.writeLine("%d + %d = %d", a, b, a + b) ;
+    }
+}

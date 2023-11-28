951942d5727f31b2ccdfd4f7cd1c2bf3cc9a0fa9
gmerveartar <gmerveartar@gmail.com>
2023-11-28 23:12:51 +0300

    Update
    Unit tests are updated

--- Libraries/UtilLib/src/test/java/com/artardev/util/numeric/NumberUtilFibonacciTest.java
+++ Libraries/UtilLib/src/test/java/com/artardev/util/numeric/NumberUtilFibonacciTest.java
@@ -1,16 +1,32 @@
 package com.artardev.util.numeric;
 
+import com.artardev.util.numeric.data.IntIntDataInfo;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
 public class NumberUtilFibonacciTest {
-
+    IntIntDataInfo dataInfo;
+    public NumberUtilFibonacciTest(IntIntDataInfo dataInfo) {
+        this.dataInfo = dataInfo;
+    }
+    @Parameterized.Parameters
+    public static Collection<IntIntDataInfo> createData()
+    {
+        return List.of( new IntIntDataInfo(1, 0),
+                        new IntIntDataInfo(2, 1),
+                        new IntIntDataInfo(3, 1),
+                        new IntIntDataInfo(4, 2),
+                        new IntIntDataInfo(5, 3));
+    }
     @Test
     public void givenValue_WhenIndex_ThenReturnsNumber()
     {
-        int input = 5;
-        int expected = 3;
-
-        Assert.assertEquals(expected, NumberUtil.fibonacciNumber(input));
+        Assert.assertEquals(dataInfo.expected, NumberUtil.fibonacciNumber(dataInfo.input));
     }
 }

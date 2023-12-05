80ad06480d484d7aabe215f2ddfd0ff191fe48fe
gmerveartar <gmerveartar@gmail.com>
2023-12-05 23:02:31 +0300

    Some test codes are added to UtilLib

---
+++ Libraries/UtilLib/src/test/java/com/artardev/util/numeric/NumberUtilAreFriendsTest.java
@@ -0,0 +1,44 @@
+package com.artardev.util.numeric;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class NumberUtilAreFriendsTest {
+    static class DataInfo {
+        int input1;
+        int input2;
+        boolean expected;
+
+        public DataInfo(int input1, int input2, boolean expected) {
+            this.input1 = input1;
+            this.input2 = input2;
+            this.expected = expected;
+        }
+    }
+
+    public NumberUtilAreFriendsTest(DataInfo dataInfo) {
+        this.dataInfo = dataInfo;
+    }
+
+    public DataInfo dataInfo;
+    @Parameterized.Parameters
+    public static Collection<DataInfo> createData()
+    {
+        return List.of(new DataInfo(220, 284, true),
+                new DataInfo(1184, 1210, true),
+                new DataInfo(2620, 2924, true),
+                new DataInfo(2, 4, false)
+        );
+    }
+    @Test
+    public void test()
+    {
+        Assert.assertEquals(dataInfo.expected,NumberUtil.areFriends(dataInfo.input1, dataInfo.input2));
+    }
+}
--- Libraries/UtilLib/src/test/java/com/artardev/util/numeric/NumberUtilPrimeFalseTest.java
+++ Libraries/UtilLib/src/test/java/com/artardev/util/numeric/NumberUtilPrimeFalseTest.java
@@ -11,14 +11,14 @@ import java.util.List;
 
 @Ignore("Tested before and passed")
 @RunWith(Parameterized.class)
-public class NumberUtilIsPrimeFalseTest {
+public class NumberUtilPrimeFalseTest {
     public long input;
     @Parameterized.Parameters
     public static Collection<Long> createData()
     {
         return List.of(-1L);
     }
-    public NumberUtilIsPrimeFalseTest(long input) {
+    public NumberUtilPrimeFalseTest(long input) {
         this.input = input;
     }
     @Test

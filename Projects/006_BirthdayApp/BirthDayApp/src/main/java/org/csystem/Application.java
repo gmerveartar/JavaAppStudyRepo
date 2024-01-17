018c32e602a6e0f4684e2f3dcf74aee3aa01cc72
gmerveartar <gmerveartar@gmail.com>
2024-01-17 20:07:49 +0300

    Update
    BirthdayApp is added

---
+++ Projects/006_BirthdayApp/BirthDayApp/src/main/java/org/csystem/Application.java
@@ -0,0 +1,29 @@
+package org.csystem;
+
+import com.karandev.io.util.console.Console;
+import org.csystem.birthday.reminder.BirthdayOperations;
+import org.csystem.birthday.reminder.BirthdayStatus;
+
+import static org.csystem.birthday.reminder.BirthdayStatus.*;
+
+public class Application {
+    private static void printBirthdayMessage(BirthdayOperations operations)
+    {
+        var status = operations.getStatus();
+        var age = operations.getAge();
+
+        switch (status) {
+            case BEFORE -> Console.writeLine("Happy birthday in advance. Your age : %.02f ", age);
+            case AFTER -> Console.writeLine("Happy belated birthday. Your age : %.02f ", age);
+            case TODAY -> Console.writeLine("Happy birthday. Your age : %.02f ", age);
+        }
+    }
+    public static void run(String[] args)
+    {
+        var day = Console.readInt("Input day : ", "Invalid day");
+        var month = Console.readInt("Input month : ", "Invalid month");
+        var year = Console.readInt("Input year : ", "Invalid year");
+
+        printBirthdayMessage(new BirthdayOperations(day, month, year));
+    }
+}

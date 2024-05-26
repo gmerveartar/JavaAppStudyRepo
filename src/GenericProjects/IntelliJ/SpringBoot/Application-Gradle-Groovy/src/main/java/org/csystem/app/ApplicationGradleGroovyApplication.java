19b710970535fd580122c6e13e8400a16241b979
gmerveartar <gmerveartar@gmail.com>
2024-05-26 12:39:11 +0300

    Update
    SpringBoot Projects

---
+++ src/GenericProjects/IntelliJ/SpringBoot/Application-Gradle-Groovy/src/main/java/org/csystem/app/ApplicationGradleGroovyApplication.java
@@ -0,0 +1,13 @@
+package org.csystem.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationGradleGroovyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationGradleGroovyApplication.class, args);
+	}
+
+}
